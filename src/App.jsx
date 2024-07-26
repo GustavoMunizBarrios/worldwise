f1b242125ae796ce6c8721a8b8042789a73ed8e4
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-07-26 15:05:34 -0600

    Implement Cities List

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,49 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Product from "./pages/Product"
 import Pricing from "./pages/Pricing"
 import Homepage from "./pages/Homepage"
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout"
+import CityList from "./components/CityList";
+
+const BASE_URL = 'http://localhost:9000'
 
 function App() {
+  const [cities, setCities] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true)
+        const res = await fetch(`${BASE_URL}/cities`)
+        const data = await res.json()
+        setCities(data)
+      } catch {
+        alert('There was an error loading data...')
+
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchCities()
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
+
         <Route index element={<Homepage />} /> {/* "index" mean Homepage is default route  */}
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>LIST OF CITIES</p>} /> {/* Default child route  */}
-          <Route path="cities" element={<p>Cities</p>}></Route>
-          <Route path="countries" element={<p>Countries</p>}></Route>
-          <Route path="form" element={<p>Form</p>}></Route>
+
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} /> {/* Default child route  */}
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+
         </Route>
         {/*  This route matches any URL that doesn't match the previous routes: */}
         <Route path="*" element={<PageNotFound />} />
---
+++ src/components/CityItem.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import styles from './CityItem.module.css'
+
+CityItem.propTypes = {
+    city: PropTypes.object,
+    cityName: PropTypes.string,
+};
+
+const formatDate = (date) =>
+    new Intl.DateTimeFormat("en", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    }).format(new Date(date));
+
+export default function CityItem({ city }) {
+
+    const { cityName, emoji, date } = city
+
+    return (
+        <li className={styles.CityItem}>
+            <span className={styles.emoji}>{emoji}</span>
+            <h3 className={styles.name}>{cityName}</h3>
+            <time className={styles.date}>{formatDate(date)}</time>
+            <button className={styles.deleteBtn}>&times;</button>
+        </li>
+    );
+}
