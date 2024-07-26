c7a50c87f8a852544e39e6cf4fd7056f76749507
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-07-25 19:26:51 -0600

    Nested Routes and Index Route

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} /> {/* "index" mean Homepage is default route  */}
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>LIST OF CITIES</p>} /> {/* Default child route  */}
+          <Route path="cities" element={<p>Cities</p>}></Route>
+          <Route path="countries" element={<p>Countries</p>}></Route>
+          <Route path="form" element={<p>Form</p>}></Route>
+        </Route>
         {/*  This route matches any URL that doesn't match the previous routes: */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
