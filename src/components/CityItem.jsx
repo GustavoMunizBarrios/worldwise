fb27856b725839768ccc7cd7a2106864eca6c4e6
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-07-31 14:13:17 -0600

    Refactor City component to use CitiesContext, add Spinner, and update CityItem and CitiesContext components

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './CityItem.module.css';
+import { useCities } from '../contexts/CitiesContext';
 
 CityItem.propTypes = {
   city: PropTypes.object,
@@ -15,13 +16,16 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 export default function CityItem({ city }) {
+  const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
 
   return (
     <li>
       {/* This is a query string: `${id}?lat=${position.lat}&lng=${position.lng}` */}
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles['cityItem--active'] : ''
+        }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{emoji}</span>
