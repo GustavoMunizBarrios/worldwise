c98b7328dbd18c913a36e27461a4274887cc8009
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-08-01 13:36:40 -0600

    Removed cities from data/cities.json, updated package-lock.json and package.json with new dependencies, and modified src/components/Map.jsx to use react-leaflet and display cities on a map.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,37 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import styles from './Map.module.css';
+import { useState } from 'react';
+import { useCities } from '../contexts/CitiesContext';
 
 export default function Map() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
-
-  const navigate = useNavigate(); // with useNavigate we can move to any URL
+  //const navigate = useNavigate(); // with useNavigate we can move to any URL
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([22.251877, -97.8395161]);
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate('form')}>
-      <h1>Map</h1>
-      <h2>
-        Position: {lat}, {lng}
-      </h2>
-      <button
-        onClick={() => {
-          setSearchParams({ lat: 23, lng: 50 });
-        }}
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
       >
-        Change pos
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
