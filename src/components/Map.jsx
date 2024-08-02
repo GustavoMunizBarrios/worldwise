9d7b00387471502996ed2db8c3445d4491c7d71b
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-08-01 20:41:28 -0600

    Updated Map component to use search parameters for map position, added useEffect to update map position, and added ChangeCenter component to set map view.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,31 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import styles from './Map.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
 
-export default function Map() {
-  //const navigate = useNavigate(); // with useNavigate we can move to any URL
+function Map() {
+  const navigate = useNavigate(); // with useNavigate we can move to any URL
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([22.251877, -97.8395161]);
+  const [searchParams] = useSearchParams();
+
+  const mapLat = searchParams.get('lat');
+  const mapLng = searchParams.get('lng');
+
+  //Effect to update the map position when search parameters change.
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng],
+  );
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -31,7 +43,15 @@ export default function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
       </MapContainer>
     </div>
   );
 }
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+export default Map;
