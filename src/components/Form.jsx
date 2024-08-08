6feb00617b6fc16aa0d19ddee8ee62d454de3224
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-08-07 20:47:36 -0600

    Updated Form component: added comment about react-datepicker package, modified DatePicker usage, and added input validation in handleSubmit function

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 import { useEffect, useState } from 'react';
-import DatePicker from 'react-datepicker';
+import DatePicker from 'react-datepicker'; //Date Picker is a package: npm i react-datepicker
 import 'react-datepicker/dist/react-datepicker.css';
 import styles from './Form.module.css';
 import Button from './Button';
@@ -66,6 +66,16 @@ function Form() {
 
   function handleSubmit(e) {
     e.preventDefault(); // to prevent the reload of the page
+    if (!cityName || !date) return;
+
+    const newCity = {
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position: { lat, lng },
+    };
   }
 
   if (isLoadingGeocoding) return <Spinner />;
@@ -89,12 +99,13 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor='date'>When did you go to {cityName}?</label>
-        {/* <input
+        {/* Date Picker is a package: npm i react-datepicker */}
+        <DatePicker
           id='date'
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
-        /> */}
-        <DatePicker />
+          onChange={(date) => setDate(date)}
+          selected={date}
+          dateFormat='dd/MM/yyyy'
+        />
       </div>
 
       <div className={styles.row}>
