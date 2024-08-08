e40f0ab3f8a65fd81177f48915704822761a02a3
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-08-07 20:25:08 -0600

    Added react-datepicker import and replaced input field with DatePicker component in Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,7 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 import { useEffect, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import styles from './Form.module.css';
 import Button from './Button';
 import BackButton from './BackButton';
@@ -87,11 +89,12 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor='date'>When did you go to {cityName}?</label>
-        <input
+        {/* <input
           id='date'
           onChange={(e) => setDate(e.target.value)}
           value={date}
-        />
+        /> */}
+        <DatePicker />
       </div>
 
       <div className={styles.row}>
