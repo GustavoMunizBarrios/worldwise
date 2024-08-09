e8acbf637d17646d6f25a2270ee86543db658852
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-08-09 09:52:52 -0600

    Updated Form component to include isLoading from useCities and added loading class to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -22,7 +22,7 @@ const BASE_URL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
 
 function Form() {
   const [lat, lng] = useUrlPosition();
-  const { createCity } = useCities();
+  const { createCity, isLoading } = useCities();
   const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
 
   const [cityName, setCityName] = useState('');
@@ -93,7 +93,10 @@ function Form() {
   if (geocodingError) return <Message message={geocodingError} />;
 
   return (
-    <form className={`${styles.form}`} onSubmit={handleSubmit}>
+    <form
+      className={`${styles.form} ${isLoading ? styles.loading : ''}`}
+      onSubmit={handleSubmit}
+    >
       <div className={styles.row}>
         <label htmlFor='cityName'>City name</label>
         <input
