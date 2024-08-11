1eea9c07ade5b48f8d9428faca18c260d0b45758
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-08-11 17:44:36 -0600

    Added AuthProvider to App, modified PageNav to include Logo and login link, and updated Homepage to use new PageNav and add class to START TRACKING NOW link

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,10 @@
-import styles from "./User.module.css";
+import styles from './User.module.css';
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
+  name: 'Jack',
+  email: 'jack@example.com',
+  password: 'qwerty',
+  avatar: 'https://i.pravatar.cc/100?u=zz',
 };
 
 function User() {
@@ -27,6 +27,7 @@ export default User;
 CHALLENGE
 
 1) Add `AuthProvider` to `App.jsx`
+
 2) In the `Login.jsx` page, call `login()` from context
 3) Inside an effect, check whether `isAuthenticated === true`. If so, programatically navigate to `/app`
 4) In `User.js`, read and display logged in user from context (`user` object). Then include this component in `AppLayout.js`
