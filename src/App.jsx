660eb80acfaf8491f40efe27c1e141cc2b05356e
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-08-13 13:35:56 -0600

    Added ProtectedRoute component to wrap AppLayout and updated App.jsx to use it

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Form from './components/Form';
 import Login from './pages/Login';
 import { CitiesProvider } from './contexts/CitiesContext';
 import { AuthProvider } from './contexts/FakeAuthContext';
+import ProtectedRoute from './pages/ProtectedRoute';
 
 function App() {
   return (
@@ -23,7 +24,14 @@ function App() {
             <Route path='product' element={<Product />} />
             <Route path='pricing' element={<Pricing />} />
             <Route path='login' element={<Login />} />
-            <Route path='app' element={<AppLayout />}>
+            <Route
+              path='app'
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               {/* Go to the CityList component ('cities' path) by default */}
               <Route index element={<Navigate replace to='cities' />} />
               {/* Default child route  */}
