54dae1cabf6fc62337c5dcea5259d68537d179cc
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-08-22 10:04:05 -0600

    Refactored App.jsx to use lazy loading for components and added Suspense with SpinnerFullPage fallback

--- src/App.jsx
+++ src/App.jsx
@@ -6,51 +6,66 @@
  *
  * The routing is set up using the `Routes` and `Route` components from `react-router-dom`.
  */
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Product from './pages/Product';
-import Pricing from './pages/Pricing';
-import Homepage from './pages/Homepage';
-import PageNotFound from './pages/PageNotFound';
-import AppLayout from './pages/AppLayout';
+import { CitiesProvider } from './contexts/CitiesContext';
+import { AuthProvider } from './contexts/FakeAuthContext';
+import ProtectedRoute from './pages/ProtectedRoute';
+
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
 import Form from './components/Form';
+import SpinnerFullPage from './components/SpinnerFullPage';
+
+/* import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Homepage from './pages/Homepage';
 import Login from './pages/Login';
-import { CitiesProvider } from './contexts/CitiesContext';
-import { AuthProvider } from './contexts/FakeAuthContext';
-import ProtectedRoute from './pages/ProtectedRoute';
+import AppLayout from './pages/AppLayout';
+import PageNotFound from './pages/PageNotFound'; */
+
+const Homepage = lazy(() => import('.pages/Homepage'));
+const Login = lazy(() => import('.pages/Login'));
+const AppLayout = lazy(() => import('.pages/AppLayout'));
+const PageNotFound = lazy(() => import('.pages/PageNotFound'));
+const Product = lazy(() => import('.pages/Product'));
+const Pricing = lazy(() => import('.pages/Pricing'));
 
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
-          <Routes>
-            {/* "index" mean Homepage is default route  */}
-            <Route index element={<Homepage />} />{' '}
-            <Route path='product' element={<Product />} />
-            <Route path='pricing' element={<Pricing />} />
-            <Route path='login' element={<Login />} />
-            <Route
-              path='app'
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              {/* Go to the CityList component ('cities' path) by default */}
-              <Route index element={<Navigate replace to='cities' />} />
-              {/* Default child route  */}
-              <Route path='cities' element={<CityList />} />
-              <Route path='cities/:id' element={<City />} />
-              <Route path='countries' element={<CountryList />} />
-              <Route path='form' element={<Form />} />
-            </Route>
-            {/*  This route matches any URL that doesn't match the previous routes: */}
-            <Route path='*' element={<PageNotFound />} />
-          </Routes>
+          <Suspense fallback={<SpinnerFullPage />}>
+            {' '}
+            {/* video 253 */}
+            <Routes>
+              {/* "index" mean Homepage is default route  */}
+              <Route index element={<Homepage />} />{' '}
+              <Route path='product' element={<Product />} />
+              <Route path='pricing' element={<Pricing />} />
+              <Route path='login' element={<Login />} />
+              <Route
+                path='app'
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                {/* Go to the CityList component ('cities' path) by default */}
+                <Route index element={<Navigate replace to='cities' />} />
+                {/* Default child route  */}
+                <Route path='cities' element={<CityList />} />
+                <Route path='cities/:id' element={<City />} />
+                <Route path='countries' element={<CountryList />} />
+                <Route path='form' element={<Form />} />
+              </Route>
+              {/*  This route matches any URL that doesn't match the previous routes: */}
+              <Route path='*' element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
